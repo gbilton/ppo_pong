97e21115ce80a09fb6a4ee8be9890dc5a4e97fa4
gbilton <56371504+gbilton@users.noreply.github.com>
2021-06-22 14:18:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+import numpy as np
+from ppo_torch import Agent
+import random
+import sys
+import random
+
+from utils import plot_learning_curve
+from pong import *
+
+if __name__ == '__main__':
+    env = make('Pong-v0')
+
+    N = 64*5
+    batch_size=64
+
+    agent = Agent(n_actions=env.num_actions,input_dims=(6,), batch_size=batch_size)
+
+
+    agent.actor.load_checkpoint()
+    agent.critic.load_checkpoint()
+
+    agent1 = Agent(n_actions=env.num_actions,input_dims=(6,))
+    agent1.actor.load_state_dict(agent.actor.state_dict())
+    agent1.actor.eval()
+
+    n_games = 100000
+
+    figure_file = 'plots/pong.png'
+
+    best_score = -1
+    score_history = []
+
+    learn_iters = 0
+    avg_score = 0
+    n_steps = 0
+    j = 0
+
+    observation = env.reset()
+
+
+    for i in range(n_games):
+        j+=1
+        observation = env.reset()
+        done = False
+        score = 0
+        while not done:
+            action, prob, val = agent.choose_action(observation)
+            inverted_observation = Tools.invert(observation)
+            bot_action = agent1.actor.act(inverted_observation)
+            for _ in range(4):
+                observation_, _, reward, done = env.step([bot_action, action])
+                if done:
+                    break
+            n_steps += 1
+            score += reward
+            agent.remember(observation, action, prob, val, reward, done)
+            if n_steps % N == 0:
+                print(random.choice(list(range(10))), end = '\r')
+                agent.learn()
+                learn_iters += 1
+            observation = observation_
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        if avg_score > best_score and j >= 100:
+            best_score = avg_score
+            agent.save_models()
+
+        if avg_score >= 0.8:
+            print('!!!!!!!!!!UPDATED!!!!!!!!!!')
+            agent.save_models()
+            for i in range(100):
+                score_history.append(0)
+            agent1.actor.load_state_dict(agent.actor.state_dict())
+            agent1.actor.eval()
+            bestscore = -1
+            j = 0
+
+
+        print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
+                'time_steps', n_steps, 'learning_steps', learn_iters, end='\r')
+    x = [i+1 for i in range(len(score_history))]
+    plot_learning_curve(x, score_history, figure_file)
---
+++ play.py
@@ -0,0 +1,44 @@
+from ppo_torch import Agent
+
+from pong import *
+
+
+if __name__ == '__main__':
+    env = make('Pong-v0')
+
+    p = DQN()
+    p.load_state_dict(torch.load('tmp/dqn/model100x3.pth'))
+    p.eval()
+
+    agent = Agent(n_actions=env.num_actions, input_dims=(env.state_size,))
+    agent.actor.load_checkpoint()
+    agent.actor.eval()
+
+    observation = env.reset()
+    score = np.array([0,0])
+    timestep = 0
+    while True:
+        env.render()
+        # keys=pygame.key.get_pressed()
+        # env.player1.key_movement(keys)
+        # env.player2.key_movement(keys)
+        action2 = agent.actor.act(observation)
+        inverted_observation = Tools.invert(observation)
+        action1 = p.act(inverted_observation)
+        # action1 = 2
+        # action1 = agent.actor.act(inverted_observation)
+        # action2, _, _ = agent.choose_action(observation)
+        # print(action2)
+        actions = [action1, action2]
+        # for i in range(4):
+        observation, _, r, done = env.step(actions)
+        timestep+=1
+        if timestep >= 10000:
+            observation = env.reset()
+            timestep = 0
+        if done:
+            if r == 1:
+                score += np.array([r,0])
+            if r == -1:
+                score += np.array([0,1])
+            print(f'score = {score}', end='\r')
