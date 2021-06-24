af8a88d3ee60e6b69193b86e90383c98e9f23fbd
gbilton <guilhermebilton@gmail.com>
2021-06-24 14:14:05 -0300

    added recording capabilities

--- play.py
+++ play.py
@@ -1,7 +1,11 @@
-from ppo_torch import Agent
+import os
 
+from ppo_torch import Agent
 from pong import *
 
+def save_imgs(win, i):
+    i = str(i).zfill(5)
+    pygame.image.save(win, f'recordings/frame_{i}.png')
 
 if __name__ == '__main__':
     env = make('Pong-v0')
@@ -18,8 +22,17 @@ if __name__ == '__main__':
 
     observation = env.reset()
     score = np.array([0,0])
+    i=0
+    record = True
     while True:
         env.render()
+        i+=1
+
+        if record:
+            save_imgs(env.win, i)
+        # with open(f'recordings/frame_{i}', 'wb') as f:
+            # f.write(frame)
+
         # keys=pygame.key.get_pressed()
         # env.player1.key_movement(keys)
         # env.player2.key_movement(keys)
@@ -33,8 +46,14 @@ if __name__ == '__main__':
         observation, _, r, done = env.step(actions)
 
         if done:
+            break
             if r == 1:
                 score += np.array([r,0])
             if r == -1:
                 score += np.array([0,1])
             print(f'score = {score}', end='\r')
+
+    if record:
+        os.system('ffmpeg -r 60 -f image2 -s 1920x1080 -i recordings/frame_%05d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p test.mp4')
+        for img in os.listdir('recordings'):
+            os.remove(f'recordings/{img}')
